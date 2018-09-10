18227f7fc1305569e6221a44f06ac291d0a2267c
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-10 13:00:21 -0400

    fixed Console.WriteLine after commenting using System; out

--- TestHello/Program.cs
+++ TestHello/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿//using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ namespace TestHello
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
